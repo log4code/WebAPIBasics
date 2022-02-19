6324c39abed5e0cad04421835d28733bcb7036c4
log4code <jt@jonathantaylor.dev>
2022-02-19 13:18:03 -0500

    Add comments to code for terminal middleware

--- Middleware/TerminalMiddleware/Program.cs
+++ Middleware/TerminalMiddleware/Program.cs
@@ -1,10 +1,14 @@
+
+// Minimal API usage to create a WebApplication builder and then build it with no additional configured services.
 var builder = WebApplication.CreateBuilder();
 var app = builder.Build();
 
+// Create an anonymous terminal RequestDelegate to handle all requests
 app.Run(async context =>
 {
     Console.WriteLine("Terminal (last) middleware");
     await context.Response.WriteAsync("Hello world!!");
 });
 
+// Run the application and respond to all requests until shutdown
 app.Run();
