650ebd62e20a21129f06d744ade0ab70bab0bc94
log4code <jt@jonathantaylor.dev>
2022-02-19 15:31:20 -0500

    Static File Middleware example.

---
+++ Middleware/StaticFileMiddleware/Program.cs
@@ -0,0 +1,16 @@
+// Minimal API usage to create a WebApplication builder and then build it with no additional configured services.
+var builder = WebApplication.CreateBuilder();
+var app = builder.Build();
+
+// static file middleware first (contents in wwwroot directory)
+app.UseStaticFiles();
+
+// Create an anonymous terminal RequestDelegate to handle all requests
+app.Run(async context =>
+{
+    Console.WriteLine("Terminal (last) middleware. Static file NOT served.");
+    await context.Response.WriteAsync("Hello world!!");
+});
+
+// Run the application and respond to all requests until shutdown
+app.Run();
