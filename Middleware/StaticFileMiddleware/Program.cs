5e52a5a32bff16d1f0d46d4c9a4b15865d070e3b
log4code <jt@jonathantaylor.dev>
2022-02-19 15:37:11 -0500

    Update code comment for static file middleware.

--- Middleware/StaticFileMiddleware/Program.cs
+++ Middleware/StaticFileMiddleware/Program.cs
@@ -5,7 +5,7 @@ var app = builder.Build();
 // static file middleware first (contents in wwwroot directory)
 app.UseStaticFiles();
 
-// Create an anonymous terminal RequestDelegate to handle all requests
+// Create an anonymous terminal RequestDelegate to handle all requests that are not static files.
 app.Run(async context =>
 {
     Console.WriteLine("Terminal (last) middleware. Static file NOT served.");
